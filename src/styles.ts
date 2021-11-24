8dd1f7a394b1ab8fa241472121b8f7c99805e5ea
Filip Nordling <nordlingfilip@gmail.com>
2021-11-24 01:07:13 +0100

    Link bg to imgur

--- src/styles.ts
+++ src/styles.ts
@@ -4,7 +4,7 @@ export const GlobalStyle = createGlobalStyle(
   ({ theme }) => css`
     body {
       line-height: 1.4;
-      background: url("./static/bf2042_wallpaper.png") ${theme.palette.black};
+      background: url("https://i.imgur.com/Cbuvtd0.png") ${theme.palette.black};
       background-size: cover;
       color: ${theme.palette.white};
       font-family: "Rajdhani", sans-serif;
